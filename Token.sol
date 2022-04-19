cf923ca02b83a2d3f94c1e750f5527cf1990a493
TVG Coin <103187750+TVGCOIN@users.noreply.github.com>
2022-04-19 17:56:37 +0530

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -2,16 +2,16 @@
 // Enable optimization
 pragma solidity ^0.5.0;
 
-import "./ERC20.sol";
-import "./ERC20Detailed.sol";
+import "./TRC20.sol";
+import "./TRC20Detailed.sol";
 
 /**
  * @title SimpleToken
- * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * @dev Very simple TRC20 Token example, where all tokens are pre-assigned to the creator.
  * Note they can later distribute these tokens as they wish using `transfer` and other
- * `ERC20` functions.
+ * `TRC20` functions.
  */
-contract Token is ERC20, ERC20Detailed {
+contract Token is TRC20, TRC20Detailed {
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
